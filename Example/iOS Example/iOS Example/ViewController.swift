313eabc387ff63a68fa1fa67aae9b292832d0389
Murilo Teixeira <murilogteixeira@gmail.com>
2021-06-18 22:18:45 -0300

    Create the example and the package

---
+++ Example/iOS Example/iOS Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Murilo Teixeira on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
